12e2e4e2e928e399acd3cc5c2252504660f24267
maamoun.krifa <maamoun.krifa@hotmail.com>
2021-10-07 21:08:49 +0100

    BDD project

--- behave_master/features/steps/dragadrop.py
+++ behave_master/features/steps/dragadrop.py
@@ -1,15 +1,27 @@
 from behave import given,when,then
+from numpy.testing import assert_equal
+from selenium import webdriver
+from selenium.webdriver import ActionChains
 
 @given(u'The user is on the dragadrop home page')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Given The user is on the dragadrop home page')
-
+    context.driver = webdriver.Chrome('c:\driver\chromedriver.exe')
+    driver=context.driver
+    driver.get("https://qavbox.github.io/demo/dragndrop/")
 
 @when(u'He drags the box to the target under "drop here" message')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: When He drags the box to the target under "drop here" message')
-
+    driver=context.driver
+    source = driver.find_element_by_id("draggable")
+    action = ActionChains(driver)
+    action.drag_and_drop_by_offset(source, 141, 41).perform()
 
 @then(u'The user should see the message "dropped !"')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Then The user should see the message "dropped !"')
+        driver=context.driver
+        msg=driver.find_element_by_id("dropText").text
+        assert_equal(msg,"Dropped!")
+        driver.quit()
+
+
+
